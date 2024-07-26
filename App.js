cb77ed8f2e3f104b37017df18ed68aef44078073
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2024-07-26 09:52:54 +0100

    project

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express, { Router } from 'express';
+import express from 'express';
 import cors from "cors";
 import connectDB from './db/connectDB.js';
 import router from './routes/index.js';
 import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
-// Add assert for JSON import
-import swagger from './docs/swagger.json'
+import fs from 'fs';
+import path from 'path';
+
+const swaggerPath = path.join(path.resolve(), './docs/swagger.json');
+const swagger = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
 
 const corsOptions = {
-    allowedHeaders: ["Authorization", "Content-Type"],
+    allowedHeaders: ["Authorization", "Content-Type" ],
     methods: ["GET", "POST", "PUT", "UPDATE", "DELETE"],
     origin: "*",
 };
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); 
 app.use(cors());
 app.use(cookieParser());
 
@@ -26,10 +29,10 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
 const start = async () => {
     try {
         await connectDB();
-        app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
+        app.listen(process.env.PORT, () => console.log(`server is listening on port ${process.env.PORT}`));
     } catch (error) {
-        console.error(error);
+        console.log(error);
     }
-};
+}
 
 start();
